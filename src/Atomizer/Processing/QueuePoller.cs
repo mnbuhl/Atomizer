cc554c5f857494c74d5c371aed2c807212e07d27
mnbuhl <mikkel.buhl@autoscout24.com>
2026-05-03 12:49:37 +0200

    refactor(01-03): rewrite QueuePoller to use ExecuteInLeaseAsync
    - Replace acquire/check/dispose leasing pattern with generic ExecuteInLeaseAsync callback
    - Remove IAtomizerLeasingScopeFactory, IAtomizerLeasingScope, leasingScope.Acquired references
    - Remove #if NETCOREAPP3_0_OR_GREATER block for await-using disposal
    - Channel-write loop preserved outside the callback (leasedJobs assigned from return value)
    - Outer timing guard (storageCheckInterval + itemsInChannel) preserved intact

--- src/Atomizer/Processing/QueuePoller.cs
+++ src/Atomizer/Processing/QueuePoller.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Channels;
+using System.Threading.Channels;
 using Atomizer.Abstractions;
 using Atomizer.Core;
 using Microsoft.Extensions.Logging;
@@ -42,7 +42,7 @@ internal class QueuePoller : IQueuePoller
 
         while (!ct.IsCancellationRequested)
         {
-            var leasedJobs = new List<AtomizerJob>();
+            List<AtomizerJob> leasedJobs = [];
 
             try
             {
@@ -52,50 +52,41 @@ internal class QueuePoller : IQueuePoller
                 if (now - _lastStorageCheck >= storageCheckInterval && itemsInChannel < queue.DegreeOfParallelism)
                 {
                     using var scope = _serviceScopeFactory.CreateScope();
-
                     _lastStorageCheck = now;
-                    var leasingScopeFactory = scope.LeasingScopeFactory;
-
-#if NETCOREAPP3_0_OR_GREATER
-                    await using var leasingScope = await leasingScopeFactory.CreateScopeAsync(
-                        queue.QueueKey,
-                        queue.VisibilityTimeout,
-                        ct
-                    );
-#else
-                    using var leasingScope = await leasingScopeFactory.CreateScopeAsync(
-                        queue.QueueKey,
-                        queue.VisibilityTimeout,
-                        ct
-                    );
-#endif
                     var storage = scope.Storage;
 
-                    if (leasingScope.Acquired)
-                    {
-                        var jobs = await storage.GetDueJobsAsync(queue.QueueKey, now, queue.BatchSize, ct);
-
-                        if (jobs.Count > 0)
+                    leasedJobs = await storage.ExecuteInLeaseAsync(
+                        queue.QueueKey,
+                        async innerCt =>
                         {
-                            _logger.LogDebug("Queue '{Queue}' leasing {JobCount} job(s)", queue.QueueKey, jobs.Count);
+                            var jobs = await storage.GetDueJobsAsync(queue.QueueKey, now, queue.BatchSize, innerCt);
+                            var acquired = new List<AtomizerJob>();
 
-                            foreach (var job in jobs)
+                            if (jobs.Count > 0)
                             {
-                                job.Lease(leaseToken, now, queue.VisibilityTimeout);
-                                leasedJobs.Add(job);
+                                _logger.LogDebug(
+                                    "Queue '{Queue}' leasing {JobCount} job(s)",
+                                    queue.QueueKey,
+                                    jobs.Count
+                                );
+
+                                foreach (var job in jobs)
+                                {
+                                    job.Lease(leaseToken, now, queue.VisibilityTimeout);
+                                    acquired.Add(job);
+                                }
+
+                                await storage.UpdateJobsAsync(acquired, innerCt);
+                            }
+                            else
+                            {
+                                _logger.LogDebug("Queue '{Queue}' found no jobs to lease", queue.QueueKey);
                             }
 
-                            await storage.UpdateJobsAsync(leasedJobs, ct);
-                        }
-                        else
-                        {
-                            _logger.LogDebug("Queue '{Queue}' found no jobs to lease", queue.QueueKey);
-                        }
-                    }
-                    else
-                    {
-                        _logger.LogDebug("Failed to acquire processing scope for queue '{Queue}'", queue.QueueKey);
-                    }
+                            return acquired;
+                        },
+                        ct
+                    );
                 }
             }
             catch (OperationCanceledException) when (ct.IsCancellationRequested)
