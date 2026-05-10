158b127e2730d30854f6911a30dc8b15d75d3a3d
mnbuhl <mikkel.buhl@autoscout24.com>
2026-05-10 22:34:19 +0200

    refactor(dashboard): remove unused NullAtomizerEventSink registration

--- src/Atomizer/Abstractions/NullAtomizerEventSink.cs
+++
@@ -1,6 +0,0 @@
-namespace Atomizer;
-
-internal sealed class NullAtomizerEventSink : IAtomizerEventSink
-{
-    public Task OnJobStateChangedAsync(AtomizerJob job, CancellationToken cancellationToken) => Task.CompletedTask;
-}
